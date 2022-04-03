38cfa2618145e59a18b4d2e3b5e5d558735307e7
Ashwani <ashwanikumar150798@gmail.com>
2022-04-03 17:53:10 +0530

    CRUD operations working with user

--- blog-backend/controllers/userController.js
+++ blog-backend/controllers/userController.js
@@ -4,50 +4,81 @@ import User from '../models/User.js'
 
 export const findUser = async (req, res) => {
   try {
-    const id = validator.escape(req.params.id)
-    console.log(id)
-    // const user = await User.findById(id)
-    // console.log(user)
+    // sanitize received data
+    const username = validator.escape(req.params.username)
 
-    // return res.status(200).json({response: user})
-    return res.status(200).json({response: 'success'})
+    // find the user with given username
+    const user = await User.findOne({ username })
+
+    // if there is no user with the given username
+    if (!user) throw new Error('No user found.')
+
+    // if there is users
+    return res.status(200).json({ response: user })
   } catch (error) {
-    return res.status(200).json({error: error.message})
+    // catch all the errors and send back
+    console.log(error.message)
+    return res.status(200).json({ error: error.message })
   }
 }
 
 
 export const getUsers = async (req, res) => {
   try {
-    return res.status(200).json({response: 'getUser'})
+    // fetch all the users
+    const users = await User.find()
+
+    // if not a single users
+    if (Array.isArray(users) && !users.length) throw new Error('No users found.')
+
+    // if there is/are user(s)
+    return res.status(200).json({ response: users })
   } catch (error) {
-    return res.status(200).json({error: error.message})
+    // catch all the errors and send back
+    console.log(error.message)
+    return res.status(200).json({ error: error.message })
   }
 }
 
 
 export const addUser = async (req, res) => {
   try {
+    // create a user object with sanitized data
+    const newUser = new User({
+      username: validator.escape(req.body.username),
+      email: validator.escape(req.body.email),
+      password: validator.escape(req.body.password),
+    })
 
-    const user = req.body
+    // save the document in the database
+    const savedUser = await newUser.save()
 
-    const newUser = new User(user)
-    const savedUser = await User.create(newUser)
-    console.log(savedUser)
-
-    return res.status(200).json({response: 'addUser'})
+    // if user saved send back
+    return res.status(200).json({ response: savedUser })
   } catch (error) {
-    return res.status(200).json({error: error.message})
+    // catch all the errors and send back
+    console.log(error.message)
+    return res.status(200).json({ error: error.message })
   }
 }
 
 
 export const deleteUser = async (req, res) => {
   try {
-    const id = validator.escape(req.params.id)
+    // sanitize received data
+    const username = validator.escape(req.params.username)
 
-    return res.status(200).json({response: 'deleteUser: ' + id})
+    // find a user by username and delete
+    const deletedUser = await User.findOneAndDelete({ username })
+
+    // if there is no user with the given username
+    if (!deletedUser) throw new Error('No user found.')
+
+    // if user is deleted successfully
+    return res.status(200).json({ response: deletedUser })
   } catch (error) {
-    return res.status(200).json({error: error.message})
+    // catch all the errors and send back
+    console.log(error.message)
+    return res.status(200).json({ error: error.message })
   }
 }
--- blog-backend/routes/userRoutes.js
+++ blog-backend/routes/userRoutes.js
@@ -3,9 +3,9 @@ const router = express.Router()
 
 import {findUser, getUsers, addUser, deleteUser} from '../controllers/userController.js'
 
-router.get('/:id', findUser)
+router.get('/:username', findUser)
 router.get('/', getUsers)
 router.post('/', addUser)
-router.delete('/:id', deleteUser)
+router.delete('/:username', deleteUser)
 
 export default router
